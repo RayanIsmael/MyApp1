81e89b8ddb4a54fd73d20bcc0898b2cfe1ae63f9
RayanIsmael <rayanalan6@gmail.com>
2021-06-12 11:32:27 +0300

    How to get data currentuser

--- lib/create/add.dart
+++ lib/create/add.dart
@@ -1,6 +1,8 @@
 import 'dart:io';
 import 'dart:math';
+import 'package:awesome_dialog/awesome_dialog.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_storage/firebase_storage.dart';
 import 'package:path/path.dart';
 import 'package:flutter/material.dart';
@@ -14,8 +16,14 @@ class Add extends StatefulWidget {
 }
 
 class _AddState extends State<Add> {
+  GlobalKey<FormState> formkey = GlobalKey<FormState>();
+  CollectionReference refnote = FirebaseFirestore.instance.collection("note");
+  Reference ref;
+  File file;
+
   var imagepicker = ImagePicker();
-  var image_url;
+  var title, note, image_url;
+  bool checkimage = false;
   @override
   Widget build(BuildContext context) {
     double mediaqury = MediaQuery.of(context).size.height;
@@ -33,36 +41,71 @@ class _AddState extends State<Add> {
             child: Column(
               children: [
                 Form(
+                    key: formkey,
                     child: Column(
-                  children: [
-                    TextFormField(
-                      textAlign: TextAlign.center,
-                      maxLength: 20,
-                      decoration: InputDecoration(
-                          labelText: "Note Title",
-                          border: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(20),
-                          ),
-                          hintText: "Note Title"),
-                    ),
-                    ///////////
-                    SizedBox(
-                      height: 20,
-                    ),
-                    TextFormField(
-                      maxLength: 200,
-                      minLines: 1,
-                      maxLines: 3,
-                      decoration: InputDecoration(
-                          labelText: "Note",
-                          border: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(20),
-                          ),
-                          hintText: "Note"),
-                    ),
-                    ///////////
-                  ],
-                )),
+                      children: [
+                        TextFormField(
+                          //////
+                          autovalidateMode: AutovalidateMode.onUserInteraction,
+                          onSaved: (newValue) {
+                            title = newValue;
+                          },
+                          validator: (value) {
+                            if (value.isEmpty) {
+                              return "Is Empty";
+                            } else if (value.length < 2) {
+                              return "Wrong Input Value";
+                            } else if (value.length > 20) {
+                              return "Wrong Input Value";
+                            } else {
+                              return null;
+                            }
+                          },
+                          ///////
+                          textAlign: TextAlign.center,
+                          maxLength: 20,
+                          decoration: InputDecoration(
+                              labelText: "Note Title",
+                              border: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(20),
+                              ),
+                              hintText: "Note Title"),
+                        ),
+                        ///////////
+                        SizedBox(
+                          height: 20,
+                        ),
+                        TextFormField(
+                          //////
+                          autovalidateMode: AutovalidateMode.onUserInteraction,
+                          onSaved: (newValue) {
+                            note = newValue;
+                          },
+                          validator: (value) {
+                            if (value.isEmpty) {
+                              return "Is Empty";
+                            } else if (value.length < 2) {
+                              return "Wrong Input Value";
+                            } else if (value.length > 200) {
+                              return "Wrong Input Value";
+                            } else {
+                              return null;
+                            }
+                          },
+                          ///////
+                          maxLength: 200,
+                          minLines: 1,
+                          maxLines: 3,
+                          decoration: InputDecoration(
+                              labelText: "Note",
+                              border: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(20),
+                              ),
+                              hintText: "Note"),
+                        ),
+                        ///////////
+                      ],
+                    )),
                 ////////////////////////
                 InkWell(
                     onTap: () {
@@ -72,7 +115,7 @@ class _AddState extends State<Add> {
                       // this containar maked by Adobe XD
                       margin: EdgeInsets.only(top: 20),
                       alignment: Alignment.center,
-                      width: 200.0,
+                      width: 250.0,
                       height: 50.0,
                       decoration: BoxDecoration(
                         borderRadius: BorderRadius.circular(21.0),
@@ -82,17 +125,27 @@ class _AddState extends State<Add> {
                           // color: Colors.green[900],//themdata
                         ),
                       ),
-                      child: Text(
-                        'Add Image For Note',
-                        style: TextStyle(
-                          fontSize: 18,
-                          color: Colors.white,
-                        ),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Text(
+                            'Add Image For Note',
+                            style: TextStyle(
+                              fontSize: 18,
+                              color: Colors.white,
+                            ),
+                          ),
+                          checkimage
+                              ? Icon(Icons.check_circle_outline_outlined,color: Colors.white)
+                              : Icon(Icons.report_problem_outlined,color: Colors.amber,),
+                        ],
                       ),
                     )),
                 /////////////////////////////
                 InkWell(
-                    onTap: () {},
+                    onTap: () async {
+                      await addnote(context);
+                    },
                     child: Container(
                       // this containar maked by Adobe XD
                       margin: EdgeInsets.only(top: 20),
@@ -146,15 +199,17 @@ class _AddState extends State<Add> {
                     var imgpick =
                         await imagepicker.getImage(source: ImageSource.gallery);
                     if (imgpick != null) {
-                      File file = File(imgpick.path);
+                      file = File(imgpick.path);
                       int random = Random().nextInt(100000);
                       String imagename =
-                          "$random" + "${basename(imgpick.path)}";
-                      Reference ref = FirebaseStorage.instance
+                          "$random" + "--" + "${basename(imgpick.path)}";
+                      ref = FirebaseStorage.instance
                           .ref("image")
                           .child(imagename);
-                      await ref.putFile(file);
-                      image_url =await  ref.getDownloadURL();
+                      setState(() {
+                        checkimage = true;
+                      });
+                      Navigator.of(context).pop();
                     }
                   },
                 ),
@@ -165,15 +220,17 @@ class _AddState extends State<Add> {
                     var imgpick =
                         await imagepicker.getImage(source: ImageSource.camera);
                     if (imgpick != null) {
-                      File file = File(imgpick.path);
+                      file = File(imgpick.path);
                       int random = Random().nextInt(100000);
                       String imagename =
-                          "$random" + "${basename(imgpick.path)}";
-                      Reference ref = FirebaseStorage.instance
+                          "$random" + "--" + "${basename(imgpick.path)}";
+                      ref = FirebaseStorage.instance
                           .ref("image")
                           .child(imagename);
-                      await ref.putFile(file);
-                      image_url =await  ref.getDownloadURL();
+                      setState(() {
+                        checkimage = true;
+                      });
+                      Navigator.of(context).pop();
                     }
                   },
                 ),
@@ -182,4 +239,41 @@ class _AddState extends State<Add> {
           );
         });
   }
+
+  //////Add Note////
+  addnote(context) async {
+    var formdata = formkey.currentState;
+    if (formdata.validate()) {
+      showDialog(context);
+      formdata.save();
+      await ref.putFile(file);
+      image_url = await ref.getDownloadURL();
+      refnote.add({
+        "title": title,
+        "note": note,
+        "image_url": image_url,
+        "user_id": FirebaseAuth.instance.currentUser.uid,
+      });
+      Navigator.of(context).pushNamed("HomePage");
+    }
+
+    ///if(formdata.validate())
+  }
+
+  //////Add Note////
+  ////Alert Dialog////
+  showDialog(context) {
+    AwesomeDialog(
+      context: context,
+      dialogType: DialogType.NO_HEADER, //more
+      animType: AnimType.SCALE, //more
+      body: Container(
+          height: 100,
+          child: Center(
+            child: CircularProgressIndicator(),
+          )),
+      ////more....
+    )..show();
+  }
+  ///////////////////
 }
--- lib/home/homepage.dart
+++ lib/home/homepage.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:myapp1/auth/login.dart';
@@ -10,20 +11,12 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  List note = [
-    {"note": "this is  home note,this is  home note", "image": "44no.png"},
-    {
-      "note": "this is  shoping note,this is  shoping note",
-      "image": "22sh.png"
-    },
-    {"note": "this is  work note,this is  work note", "image": "33w.png"},
-    {"note": "this is  fixcar note,this is  fixcar note", "image": "11c.png"},
-  ];
 /////*************************************/////
   GlobalKey<ScaffoldState> scaffoldkey = GlobalKey();
   /////*************************************/////
   FirebaseAuth auth = FirebaseAuth.instance;
   /////*************************************/////
+  Map<String, dynamic> noteinfo;
   @override
   void initState() {
     auth.authStateChanges().listen((user) {
@@ -58,7 +51,7 @@ class _HomePageState extends State<HomePage> {
     return Scaffold(
       key: scaffoldkey,
       appBar: AppBar(
-        title: Text('Home Page'),
+        title: Text('HOME PAGE'),
         titleSpacing: 2,
         centerTitle: true,
         actions: [
@@ -69,16 +62,75 @@ class _HomePageState extends State<HomePage> {
               icon: Icon(Icons.logout_rounded))
         ],
       ),
-      body: Container(
-        padding: EdgeInsets.all(20),
-        child: ListView.builder(
-            itemCount: note.length,
-            itemBuilder: (context, i) {
-              return MyList(
-                note: note[i],
-              );
-            }),
+      ////////
+      body: StreamBuilder<QuerySnapshot<Map<String, dynamic>>>(
+        stream: FirebaseFirestore.instance
+            .collection("note")
+            .where("user_id", isEqualTo: FirebaseAuth.instance.currentUser.uid)
+            .snapshots(),
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            return ListView.builder(
+              itemCount: snapshot.data.docs.length,
+              itemBuilder: (context, index) {
+                return Column(
+                  children: [
+                    Card(
+                      child: Row(
+                        children: [
+                          /////////////////////
+                          Expanded(
+                              flex: 1,
+                              child: Image.network(
+                                snapshot.data.docs[index].data()["image_url"],
+                                height: 60,
+                                width: 40,
+                                fit: BoxFit.fill,
+                              )),
+                          ////////////////////
+                          Expanded(
+                            flex: 4,
+                            child: ListTile(
+                              title: Text(
+                                  snapshot.data.docs[index].data()["title"]),
+                              subtitle: Text(
+                                  snapshot.data.docs[index].data()["note"]),
+                              trailing: Wrap(spacing: 0, children: <Widget>[
+                                IconButton(
+                                    onPressed: () {},
+                                    icon: Icon(
+                                      Icons.drive_file_rename_outline_outlined,
+                                      color: Theme.of(context).primaryColor,
+                                    )),
+                                IconButton(
+                                    onPressed: () {},
+                                    icon: Icon(
+                                      Icons.delete_rounded,
+                                      color: Colors.red[300],
+                                    ))
+                              ]),
+                            ),
+                          ),
+                          ////////////////
+                        ],
+                      ),
+                    ),
+                    ///////////////////
+                    SizedBox(
+                      height: 10,
+                    )
+                  ],
+                );
+              },
+            );
+          }
+          if (snapshot.hasError) {
+            return Text("Error");
+          }
+          return CircularProgressIndicator();
+        },
       ),
+      ///////
       floatingActionButton: FloatingActionButton(
         backgroundColor: Theme.of(context).primaryColor,
         child: Icon(
@@ -90,60 +142,6 @@ class _HomePageState extends State<HomePage> {
           Navigator.of(context).pushNamed("AddNote");
         },
       ),
-    );
-  }
-}
-
-class MyList extends StatelessWidget {
-  final note;
-  MyList({this.note});
-
-  @override
-  Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Card(
-          child: Row(
-            children: [
-              /////////////////////
-              Expanded(
-                  flex: 1,
-                  child: Image.asset(
-                    "images/${note['image']}",
-                    height: 40,
-                    width: 40,
-                  )),
-              ////////////////////
-              Expanded(
-                flex: 4,
-                child: ListTile(
-                  title: Text("Title"),
-                  subtitle: Text("${note['note']}"),
-                  trailing: Wrap(spacing: 0, children: <Widget>[
-                    IconButton(
-                        onPressed: () {},
-                        icon: Icon(
-                          Icons.drive_file_rename_outline_outlined,
-                          color: Theme.of(context).primaryColor,
-                        )),
-                    IconButton(
-                        onPressed: () {},
-                        icon: Icon(
-                          Icons.delete_rounded,
-                          color: Colors.red[300],
-                        ))
-                  ]),
-                ),
-              ),
-              ////////////////
-            ],
-          ),
-        ),
-        ///////////////////
-        SizedBox(
-          height: 10,
-        )
-      ],
     );
   }
 }
